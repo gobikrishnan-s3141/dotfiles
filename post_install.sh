c42b41edd89cda970471899bc253137ab7f8d0d6
gobikrishnan-s3141 <139465475+gobikrishnan-s3141@users.noreply.github.com>
2024-01-18 20:30:00 +0000

    Update post_install.sh

--- post_install.sh
+++ post_install.sh
@@ -6,9 +6,20 @@
 
 su
 pacman -S sudo sway swaybg seatd foot bemenu-wayland mpv pipewire-alsa wireplumber alsa-utils iftop htop git acpi tlp curl wget e2fsprogs
-
 useradd -m -g zero wheel,seat
-
 passwd zero
-
 systemctl enable seatd
+
+exit
+
+# dir & aur_helper
+
+mkdir zero
+cd zero
+sudo pacman -S --needed base-devel
+git clone https://aur.archlinux.org/paru.git
+cd paru
+makepkg -si
+cd
+
+reboot
